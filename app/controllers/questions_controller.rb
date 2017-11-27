9410dd16027e9a6d43a3832c1ec569e3dddd7c24
Andrea dAlessandro <dalexandrea@gmail.com>
2017-11-27 12:12:34 +0100

    Stupid Coach Complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,12 @@
 class QuestionsController < ApplicationController
   def answer
+    @query = params[:query]
+    if @query.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @answer = "I am going to work right now!"
+      ""
+    else @answer = "I don't care, get dressed and go to work!"
+    end
   end
 
   def ask
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get '/answer', to:'questions#answer'
 
+  get '/ask', to:'questions#ask'
 
-  get 'questions/answer'
-
-  get 'questions/ask'
+  get '/', to:'questions#ask'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
